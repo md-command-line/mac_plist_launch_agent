17c3fccdf38322c4bddcbbe34654897a6460662a
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-07-01 22:43:55 -0400

    provide test using .tmp; update plist through .tmp folder;

--- app/install_driver.sh
+++ app/install_driver.sh
@@ -1,17 +1,24 @@
-#!/bin/bash
+# !/bin/bash
 
 # write out path needed
-pwd=$PWD
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
+
+#create .tmp file and copy to temp
+mkdir $DIR/.tmp
+cp $DIR/com.example.hello.plist $DIR/.tmp/com.example.hello.plist
+
+# create the string needed.
 pwd2="              <string>$pwd/app/./script.sh</string>"
 echo $pwd2
 
 # insert path needed into correct file.
 # line 9 in com.example.hello.plist
-sed -i '' '9s?.*?'"$pwd2"'?' app/com.example.hello.plist
+sed -i '' '9s?.*?'"$pwd2"'?' $DIR/.tmp/com.example.hello.plist
 
-# change environment appropriately 
- chmod 644 app/com.example.hello.plist
+# change environment appropriately
+chmod 644 $DIR/.tmp/com.example.hello.plist
 
- cp app/com.example.hello.plist ~/Library/LaunchAgents/
+cp $DIR/.tmp/com.example.hello.plist ~/Library/LaunchAgents/
 
- launchctl load ~/Library/LaunchAgents/com.example.hello.plist
+launchctl load ~/Library/LaunchAgents/com.example.hello.plist
