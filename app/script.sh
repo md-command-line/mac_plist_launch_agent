e4e6e824b8ba2a83b2fb16b1c59d7df2272752bd
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-07-04 15:42:30 -0400

    better generic application starting script and better instructions;

--- app/script.sh
+++ app/script.sh
@@ -1,1 +1,6 @@
-say "hello world"
+# !/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+file_path=$DIR/../../
+echo $file_path
+say "if corectly installed this will be the appliction path, $file_path"
