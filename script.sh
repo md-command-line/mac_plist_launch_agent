f3256b25e7252f0cb388805089e1f2976999d6f7
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-11-11 14:00:22 -0500

    🚀  vanilla aerial_desktop

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 # !/bin/bash
+  
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-file_path=$DIR/../../
-echo $file_path
-say "if corectly installed this will be the appliction path, $file_path"
+/System/Library/Frameworks/ScreenSaver.framework/Resources/./ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background
+
